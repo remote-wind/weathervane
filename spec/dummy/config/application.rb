f9608b05fdfb0c22d035ffa08ff0d2fa0312fcc6
Max Calabrese <max.calabrese@ymail.com>
2013-12-16 15:11:08 +0100

    get rid of I18N warning

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -12,8 +12,6 @@ require "weathervane"
 
 module Dummy
   class Application < Rails::Application
-
-
     config.i18n.enforce_available_locales = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
