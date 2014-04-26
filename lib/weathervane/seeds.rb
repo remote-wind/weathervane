8c5f0df54324d16d4b4358e5a8c22231e7780754
Max Calabrese <max.calabrese@ymail.com>
2014-04-26 12:40:09 +0200

    seeds no longer uses FactoryGirl factories

--- lib/weathervane/seeds.rb
+++ lib/weathervane/seeds.rb
@@ -1,18 +1,11 @@
-require 'factory_girl'
-
 module Weathervane
-
   module Seeds
-
     class Providers
-
       def self.seed *args
         ::Providers::SMHI.create(
             name: 'SMHI'
         )
       end
-
     end
   end
-
 end
